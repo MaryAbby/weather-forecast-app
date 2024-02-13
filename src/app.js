4484dcfe6541ce96c050cf3bb7ae363a266f208d
MaryAbby <136010973+MaryAbby@users.noreply.github.com>
2024-02-13 13:54:42 +0300

    added search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
+function cityTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let city = document.querySelector("#city-name");
+  city.innerHTML = response.data.city.toUpperCase();
+  temperatureElement.innerHTML = Math.round(temperature) + "°";
+}
+
+function searchCity(city) {
+  let apiKey = "1a747f2d7ac32a100bt13fab8776o6ca";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(cityTemperature);
+}
+
 function searchForm(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let city = document.querySelector(".city-name");
-  city.innerHTML = searchInput.value.toUpperCase();
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchForm);
+
+searchCity("Kampala");
