2b4bf7df01cddf444436bce40862c41004a2982b
MaryAbby <136010973+MaryAbby@users.noreply.github.com>
2024-02-13 17:27:49 +0300

    formatted other weather parameters

--- src/app.js
+++ src/app.js
@@ -2,8 +2,70 @@ function cityTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let city = document.querySelector("#city-name");
+  let descriptionElement = document.querySelector("#weather-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#windspeed");
+  let timeElement = document.querySelector("#time");
+  let dateElement = document.querySelector("#date");
+  let dayElement = document.querySelector("#day");
+  let date = new Date(response.data.time * 1000);
+
   city.innerHTML = response.data.city.toUpperCase();
-  temperatureElement.innerHTML = Math.round(temperature) + "°";
+  temperatureElement.innerHTML = `${Math.round(temperature)}°`;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windspeedElement.innerHTML = response.data.wind.speed;
+  timeElement.innerHTML = formatTime(date);
+  dateElement.innerHTML = formatMonth(date).toUpperCase();
+  dayElement.innerHTML = formatDate(date).toUpperCase();
+}
+function formatTime(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
+function formatDate(date) {
+  let days = [
+    "sunday",
+    "monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}`;
+}
+
+function formatMonth(date) {
+  let monthDay = date.getDate();
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[date.getMonth()];
+
+  return `${monthDay} ${month}`;
 }
 
 function searchCity(city) {
